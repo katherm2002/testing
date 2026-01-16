91f4686f71f6e9005dd9ee42c9aecc6962ef9e23
katherm2002 <katherm@metropolia.fi>
2026-01-16 15:35:18 +0200

    3

--- mass md.2.py
+++ mass md.2.py
@@ -1,11 +1,12 @@
-talents = input("Enter your talents: ")
-pounds = input("Enter your pounds: ")
-lots = input("Enter your lots: ")
+talents = float(input("Enter talents:"))
+pounds = float(input("Enter pounds:"))
+lots = float(input("Enter lots:"))
+
 
 total_lots = talents * 20 * 32 + pounds * 32 + lots
-total_grams =total_lots * 13.3
+total_grams = total_lots * 13.3
 total_kilograms =int(total_grams //  1000)
 grams=total_grams % 1000
 
 print("The weight in modern units:")
-print(kilograms, "kilograms and", grams, "grams.")
+print(total_kilograms, "kilograms and", grams, "grams.")
---
+++ random md2.py
@@ -0,0 +1,16 @@
+import random
+
+a= random.randint(0,9)
+b= random.randint(0,9)
+c= random.randint(0,9)
+
+code3 = str(a)+str(b)+str(c)
+print("3-digit code;", code3)
+
+d= random.randint(1,6)
+e= random.randint(1,6)
+f= random.randint(1,6)
+g= random.randint(1,6)
+
+code4 = str(d)+str(e)+str(f)+str(g)
+print("4-digit code;", code4)
