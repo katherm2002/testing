9ef6ac05877dd465bc9ad9b8ebf4957dcdf93048
katherm2002 <katherm@metropolia.fi>
2026-01-20 14:11:46 +0200

    first

---
+++ HOMEWORK md3.py
@@ -0,0 +1,60 @@
+#Exercise 1, Fisher
+# length=float(input("enter the length of a zander in cm: "))
+# if length>=42:
+#     print("The zander fulfills the size limit")
+# else:
+#     length_left= 42 - length
+#     print(f"Release the fish back into the lake, the fish was {length_left} centimeters below the size limit.")
+
+
+# Exercise 2, cruise ship
+# cabin_class = input("Enter your cabin class: ").upper()
+# if cabin_class == "LUX":
+#     print("upper-deck cabin with a balcony")
+# elif cabin_class == "A":
+#     print("above the car deck, equipped with a window")
+# elif cabin_class == "B":
+#     print("windowless cabin above the car deck")
+# elif cabin_class == "C":
+#     print("windowless cabin below the car deck")
+# else:
+#     print("Invalid cabin class.")
+
+
+# Exercise 3, biological gender
+# biological_gender = (input("Enter your biological gender (female,male): "))
+# hemoglobin_value = int(input("Enter your hemoglobin value: "))
+#
+# if biological_gender == "female":
+#     if hemoglobin_value < 177:
+#         print("Your hemoglobin value is low")
+#     elif hemoglobin_value <= 155:
+#         print("Your hemoglobin value is normal")
+#     else:
+#         print("Your hemoglobin value is high")
+#
+# elif biological_gender == "male":
+#     if hemoglobin_value < 134:
+#         print("Your hemoglobin value is low")
+#     elif hemoglobin_value <= 167:
+#         print("Your hemoglobin value is normal")
+#     else:
+#         print("Your hemoglobin value is high")
+
+# Exercise 4, years
+# year = int(input("Enter the year: "))
+#
+# if year % 4 == 0:
+#     print("Leap Year")
+#
+# elif year % 100 == 0:
+#     print("NOT leap Year")
+# elif year % 400 == 0:
+#     print("leap Year")
+#
+# else:
+#     print("NOT leap Year")
+
+
+
+
