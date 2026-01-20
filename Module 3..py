1e9ae6de48f1529c57cf4da09d79c9ba52e3ecae
katherm2002 <katherm@metropolia.fi>
2026-01-20 11:01:16 +0200

    first

---
+++ Module 3..py
@@ -0,0 +1,59 @@
+# medicine example
+# age = int(input("Enter age of the patient: "))
+# if 15 <= age < 18:
+#     weight = float(input("Enter weight (kg): "))
+# if (age >=18 or age>=15 and weight >=55):
+#     print("The medicine can be used.")
+#
+# else:
+#     print("The medicine cannot be used.")
+
+# Finnish parliamentary elections
+# age= int(input("enter your age : "))
+# if age >= 18:
+#     print("you are old enough to vote in Finnish parliamentary elections")
+# else:
+#     years_left = 18 - age
+#     print("you are not old enough to vote in Finnish parliamentary elections")
+#     print("you can vote in", years_left, "years.")
+
+#Fisher exercise (Homework)
+# length=float(input("enter the length of a zander in cm:"))
+# if length>=42:
+#     print("The zander fulfills the size limit")
+# else:
+#     length_left= 42 - length
+#     print("Release the fish back into the lake")
+#     print("and the fish was", length_left,"centimeters below the size limit.")
+
+#Practice exercise #2 electricity
+# electricity = float(input("Please enter electricity consumption in kWh: "))
+# if electricity <= 50:
+#     bill = electricity * 0.10
+#
+# elif 50 <= electricity <= 200:
+#     bill = (50 * 0.10) + ((electricity - 50) * 0.08)
+#
+# else :
+#     bill = (50 * 0.10) + (150 * 0.08) + ((electricity - 200) * 0.06)
+#     print(f"Your bill is €{bill:.2f}")
+
+# #Scholarship exercise
+# physics=int(input("enter your score in physics "))
+# mathematics=int(input("enter your score in mathematics "))
+# chemistry=int(input("enter your score in chemistry "))
+# if physics<50 or mathematics<50 or chemistry<50:
+#     print("You can't receive scholarship")
+#
+# elif (physics>90 and mathematics>90) or chemistry>95:
+#     print("You receive scholarship, congratulations!")
+#
+# else:
+#     print("You did not receive a scholarship")
+#
+
+
+
+
+
+
