f7886991197a32c4e0330a09507dc75602a1b7ef
katherm2002 <katherm@metropolia.fi>
2026-01-27 10:43:42 +0200

    1

---
+++ Module5/class exercises.py
@@ -0,0 +1,79 @@
+# names = ['apples','oranges','bananas','grapes','kiwi']
+# print(names[0])
+# print(names[-1])
+# print(names[1:3])
+# print(names)
+# print(names[2:])
+# print(len(names))
+
+
+
+# names = []
+#
+# name = input("Enter the first name or quit by pressing Enter: ")
+# while name!="":
+#     names.append(name)
+#     name = input("Enter the next name or quit by pressing Enter: ")
+#
+# print(names)
+
+# names = []
+#
+# name = input("Enter the first name or quit by pressing Enter: ")
+# while name!="":
+#     names.append(name)
+#     name = input("Enter the next name or quit by pressing Enter: ")
+#
+# for n in names:
+#     print(f"Hello, {n}!")
+# for loops exercises
+# numbers=[]
+# number=(input("Enter a number or quit by pressing Enter: "))
+# while number!="":
+#     numbers.append(number)
+#     number =(input("Enter a number or quit by pressing Enter: "))
+#
+# for n in numbers:
+#     print(n)
+
+
+
+# user_number=int(input("Enter a number: "))
+# if user_number<= 0:
+#     print("Error: the number must be positive.")
+# else:
+#     for i in range(0,user_number +1,2):
+#         print(i)
+
+
+
+# numbers=[]
+# number=(input("Enter a number or quit by pressing Enter: "))
+# while number!="":
+#     numbers.append(number)
+#     number =(input("Enter a number or quit by pressing Enter: "))
+#
+# for n in numbers:
+#     print(n)
+
+numbers=[]
+user_number = (input("Enter number, empty to quit: "))
+while user_number!= '':
+    number=int(user_number)
+    numbers.append(int(user_number))
+    user_number = (input("Enter number, empty to quit: "))
+
+unique_number=[]
+
+for n in numbers:
+    if n > 100 and n not in unique_number:
+        unique_number.append(n)
+        print(n)
+
+
+
+
+
+
+
+
---
+++ Module5/exercise1.py
@@ -0,0 +1,16 @@
+# import random
+#
+# dice_count = int(input("How many dice to roll? "))
+# total = 0
+#
+# for i in range(dice_count):
+#     roll = random.randint(1, 6)
+#     total += roll
+#
+# print(f"The sum of the dice is: {total}")
+
+import random
+coin_flip=int(input("How many times do you want me to flip a coin?: "))
+total=0
+for i in range(coin_flip):
+    flip=
