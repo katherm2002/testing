2faa3836ce22c424dabb0816bf46a1dd6fb8253e
katherm2002 <katherm@metropolia.fi>
2026-03-24 19:33:45 +0200

    10

--- module 11/exercise 1.py
+++ module 11/exercise 1.py
@@ -23,4 +23,4 @@ magazine = Magazine("Donald Duck", "Aki Hyyppä")
 book = Book("Compartment No. 6", "Rosa Liksom", 192)
 magazine.print_information()
 print()
-book.print_information
+book.print_information()
